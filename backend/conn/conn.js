d8d016bf72c5a9dd7214393ca0cd0e7ae59bf443
Nivedhass <128232040+Nivedhass@users.noreply.github.com>
2024-12-29 15:24:57 +0530

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const conn = async() =>{
     try{
         
-        const response = await mongoose.connect('mongodb+srv://ssn367480:7BB5e9e@cluster0.kz6b1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        const response = await mongoose.connect('your mongo link')
         if (response){
             console.log("connected to DB")
         }
